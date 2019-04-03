b1d09078f9ad15c3220fe028cb3943b6072fa2cf
ayman-93 <aymanfaisal.cs@gmail.com>
2019-04-03 17:06:31 +0300

    fetch for providers

--- src/components/SubCatigory.js
+++ src/components/SubCatigory.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class SubCatigory extends Component {
   render() {
     const { subCat } = this.props.match.params;
+    const users = ["aa", "bb", "cc", "dd", "ee"];
     return <h1>{subCat}</h1>;
   }
 }
